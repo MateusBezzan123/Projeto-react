aebe0a1c2d792d0fc1a515add966f488d94b03c6
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-16 19:12:38 -0300

    jdshkjdhsja

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const app = express();
 
-//GET,POST,PUT,DELETE
-//req.query = acessar query params(para filtros)
-//req.params = Acessar route params (para edição,delete)
-//req.body = Acessar corpo da requisição(para criação,edição)
+
+
+app.use(express.json());
 
 app.post('/users', (req, res) => {
   return res.json(req.body);
