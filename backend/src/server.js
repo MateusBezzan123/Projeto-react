af847c92a476031a7673bff7ad033da0ac487e75
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-16 18:49:11 -0300

    Anotações

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
+
+//GET,POST,PUT,DELETE
+
+
+app.post('/users', (req, res) => {
   return res.json({ message: 'Hello World' });
 
 });
