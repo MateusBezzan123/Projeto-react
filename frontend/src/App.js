450d712c32b46eacd92e18bd3eeaf669a95e2863
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-18 11:28:35 -0300

    Tela inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
 
 function App() {
   return (
-    <h1>HELLO WORLD</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC"></img>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encotre <strong>talentos</strong> para a sua empresa
+        </p>
+        <form>
+          <label htmlFor="email">Email*</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail"></input>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+
   );
 }
 
