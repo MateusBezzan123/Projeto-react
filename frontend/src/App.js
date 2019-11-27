1706b4866a4517b2cc47cd63c492ea89241b8f01
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-27 14:45:40 -0300

    Rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 import './App.css';
-import api from './services/api';
+
+
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event) {
-    event.preventDefault()
-    //email
-    const response = await api.post('/sessions', { email });
-    //console.log(response);
-    const { _id } = response.data
-    // console.log(_id);
-    localStorage.setItem('user', _id);
-
-  }
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"></img>
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encotre <strong>talentos</strong> para a sua empresa
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email*</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" value={email}
-            onChange={event => setEmail(event.target.value)}></input>
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
 
